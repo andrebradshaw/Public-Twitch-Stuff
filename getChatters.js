41a8a50fb80d85ae47e72c01809f8d03ed163754
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-06-01 17:17:15 -0400

    Update getChatters.js

--- getChatters.js
+++ getChatters.js
@@ -1,8 +1,21 @@
-async function getChatters(){
-var res = await fetch("https://tmi.twitch.tv/group/user/mathisonprojects/chatters");
-var j = await res.json();
-console.log(j.chatters.viewers);
+var ele = (t) => document.createElement(t);
+var attr = (o, k, v) => o.setAttribute(k, v);
+
+var reg = (o, n) => o ? o[n] : '';
+var cn = (o, s) => o ? o.getElementsByClassName(s) : console.log(o);
+var tn = (o, s) => o ? o.getElementsByTagName(s) : console.log(o);
+var gi = (o, s) => o ? o.getElementById(s) : console.log(o);
+
+var unq = (arr) => arr.filter((e, p, a) => a.indexOf(e) == p);
+
+async function getChatters(channel) {
+  var res = await fetch("https://tmi.twitch.tv/group/user/" + channel + "/chatters");
+  var j = await res.json();
+  return j.chatters.viewers;
 }
-
-
-getChatters()
+async function viewChatters() {
+  var chan = reg(/(?<=twitch\.tv\/).+/.exec(window.location.href), 0);
+  var res = await getChatters(chan);
+  console.log(res);
+}
+viewChatters()
