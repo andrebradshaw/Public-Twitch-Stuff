66b73520e2932b036289006f30d1f4e9c7741bfd
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-08-25 21:16:06 -0400

    Update chatSearch2.js

--- chatSearch2.js
+++ chatSearch2.js
@@ -116,17 +116,18 @@ function dragElement() {
   }
 }
 
-function aninCloseBtn(){
-  var l1 = gi(document,'close-layer-1');
-  var l2 = gi(document,'close-layer-2');
+function aninCloseBtn() {
+  var l1 = tn(this, 'path')[0];
+  var l2 = tn(this, 'path')[1];
   l1.style.transform = "translate(49px, 50px) rotate(45deg) translate(-49px, -50px)";
   l1.style.transition = "all 333ms";
   l2.style.transform = "translate(49px, 50px) rotate(135deg) translate(-49px, -50px)";
   l2.style.transition = "all 333ms";
 }
-function anoutCloseBtn(){
-  var l1 = gi(document,'close-layer-1');
-  var l2 = gi(document,'close-layer-2');
+
+function anoutCloseBtn() {
+  var l1 = tn(this, 'path')[0];
+  var l2 = tn(this, 'path')[1];
   l1.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
   l1.style.transition = "all 333ms";
   l2.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
@@ -164,19 +165,55 @@ function search(){
   var str = searchInput.value.trim();
   if(str.length > 2){
     var arr = gi(document,'circleSwitchStatus').getAttribute('status') == 'pos' ? currentChatText.filter(el=> booleanSearch(str,el) || booleanSearch(str,el)) : currentChatText.filter(el=> booleanSearch(str,el) === false && booleanSearch(str,el) === false);
-    creatSearchResultsHTML(parseChatterObj(arr),parseChatterObj(chatterData));
+
+    var ats = arr.map( el=> el.match(/(?<=@)[a-zA-Z]+/g) ).filter(el => el ).flat(); /* not in use, but this captures all the ats in the search response.*/
+
+    creatSearchResultsHTML(parseChatterObj(arr),parseChatterObj(chatterData),ats);
+  }else{
+    gi(document,'res_content_container').innerHTML = '';
   }
 }
 
-var t_color = {purpleBlack: '#0f0e11', navyPurple: '#19171c', darkPurple: '#2c2541', purpleLightGrey: '#dad8de', purple: '#392e5c', lightPurple: '#7d5bbe', brightPurple: '#885dd9'};
+var t_color = {purpleBlack: '#0f0e11', navyPurple: '#19171c', darkPurple: '#2c2541', purpleLightGrey: '#dad8de', purple: '#392e5c', lightPurple: '#7d5bbe', brightPurple: '#885dd9', onGreen: '#07ba5b', offRed: '#e21212'};
+
+
+var svgs = {
+	close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="${t_color.offRed}" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`
+};
+
+function createEle(obj, parent) {
+  if (obj.attr && obj.attr.id)
+    if (gi(document, obj.attr.id)) gi(document, obj.attr.id).outerHTML = '';
+  var cont = ele(obj.tag);
+  obj.attr ? Object.entries(obj.attr).forEach(el => attr(cont, el[0], el[1])) : '';
+  obj.styles ? Object.entries(obj.styles).forEach(el => cont.style[el[0]] = el[1]) : '';
+  parent.appendChild(cont);
+  obj.text ? cont.innerText = obj.text : '';
+  obj.innerHTML ? cont.innerHTML = obj.innerHTML : '';
+  obj.outerHTML ? cont.outerHTML = obj.outerHTML : '';
+  return cont;
+}
+
+
+var mainCont = {
+  tag: 'div',
+  attr: {
+    id: 'saved_msg_search_cont'
+  },
+  styles: {
+	position: 'fixed',
+	top: `${Math.round(screen.height * 0.15)}px`,
+	width: `${Math.round(screen.width * 0.25)}px`,
+	right: `2%`,
+	zIndex: `111111`,
+	borderRadius: `0.15em`
+  }
+};
+
 
 function createChatSearchHTML(){
   if (gi(document, 'saved_msg_search_cont')) gi(document, 'saved_msg_search_cont').outerHTML = "";
-
-  var cont = ele('div');
-  attr(cont, 'id', 'saved_msg_search_cont');
-  document.body.appendChild(cont);
-  attr(cont, 'style', `position: fixed; top: ${Math.round(screen.height * 0.15)}px; width: ${Math.round(screen.width * 0.25)}px; right: 2%; z-index: 111111; border-radius: 0.15em;`);
+  var cont = createEle(mainCont,document.body);
 
     var head = ele('div');
     cont.appendChild(head);
@@ -191,8 +228,7 @@ function createChatSearchHTML(){
     var cls = ele('div');
     head.appendChild(cls);
     attr(cls, 'style', 'grid-area: 1 / 2; width: 27px; height: 27px; cursor: pointer;');
-    cls.innerHTML = `<svg x="0px" y="0px" viewBox="0 0 100 100">
-<g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" id="close-layer-1" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" id="close-layer-2" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`;
+    cls.innerHTML = svgs.close;
     cls.onmouseenter = aninCloseBtn;
     cls.onmouseleave = anoutCloseBtn;
     cls.onclick = closeView;
@@ -207,14 +243,14 @@ function createChatSearchHTML(){
     cont.appendChild(r_bod);
 
     var input_cont = ele('div');
-    attr(input_cont,'style','width: 100%; max-height: 30px; display: grid; grid-template-columns: 92% minmax(13px, 16px); grid-gap: 1px; justify-content: center; background: transparent; padding: 6px;');
+    attr(input_cont,'style','width: 100%; height: 50px; display: grid; grid-template-columns: 95% 5%; grid-gap: 1px; justify-content: center; background: transparent; padding: 6px;');
     attr(input_cont,'id','saved_msg_input_container');
     c_bod.appendChild(input_cont);
 
         var s_input = ele('input');
         attr(s_input,'id','saved_msg_search_input');
         attr(s_input,'placeholder','Search chat');
-        attr(s_input,'style',`background: ${t_color.navyPurple}; width: 100%; max-height: 24px; grid-area: 1 / 1; border: 1px solid ${t_color.purple}; border-radius: 0.25em; padding: 3px; color: #4d7828;`);
+        attr(s_input,'style',`background: ${t_color.navyPurple}; width: 100%; height: 35px; grid-area: 1 / 1; border: 1px solid ${t_color.purple}; border-radius: 0.25em; padding: 3px; color: #4d7828; font-size: 1.4em;`);
         input_cont.appendChild(s_input);
         s_input.onkeyup = search;
 
@@ -231,27 +267,76 @@ function createChatSearchHTML(){
 
 }
 
-function creatSearchResultsHTML(arr,users){
+
+
+function creatSearchResultsHTML(arr,users,ats){
   var parent = gi(document,'res_content_container');
   gi(document,'res_content_container').innerHTML = '';
-  if(arr.length > 7) {attr(parent,'style',`max-height: 600px; overflow-y: scroll;`);}else{attr(parent,'style',`height: 600px; overflow-y: hidden;`);}
+
+  var resHead = {
+    tag: 'div',
+    attr: {style: `display: grid; grid-template-columns: 95% 5%; background: ${t_color.purpleBlack}; padding: 1px;`}
+  };
+
+  var resBod = {
+    tag: 'div',
+    attr: {
+      style: arr.length > 7 ? `max-height: 600px; overflow-y: scroll;` : `height: 600px; overflow-y: hidden;`
+    }
+  };
+
+  var reslabel = {
+    tag: 'div',
+ 	attr: {style: 'text-align: center;'},
+    text: 'search results'
+  };
+
+  var resClose = {
+    tag: 'div',
+    attr: {
+      id: 'log_closer'
+    },
+    styles: {
+      width: '20px',
+      height: '20px',
+      cursor: 'pointer',
+      gridArea: '1/2'
+    },
+    innerHTML: svgs.close
+  };
+
+  var hd = createEle(resHead,parent);
+  var lbl = createEle(reslabel,hd);
+  var cls = createEle(resClose,hd);
+  cls.onclick = ()=> parent.innerHTML = '';
+  cls.onmouseenter = aninCloseBtn;
+  cls.onmouseleave = anoutCloseBtn;
+
+
+  var bod = createEle(resBod,parent);
+
   for(var i=0; i<arr.length; i++){
+    var msg = arr[i].chat.trim();
+
     var searchResCont = ele('div'); 
     attr(searchResCont,'class','search-results-list');
-    attr(searchResCont,'style',`width: 100%; background: ${t_color.navyPurple}; padding: 10px;`);
-    parent.appendChild(searchResCont);
+    attr(searchResCont,'style',`width: 100%; background: ${t_color.navyPurple}; padding: 10px; border-bottom: 1px dashed ${t_color.purpleBlack};`);
+    bod.appendChild(searchResCont);
 
     var userColor = users.filter(el=> arr[i].user == el.user)[0].chat;
 
-        var user= ele('span');
-        attr(user,'style',`padding 2px; color: ${t_color.purpleLightGrey}; color: ${userColor}`);
-        user.innerText = arr[i].user+': ';
- 		searchResCont.appendChild(user);
+    if(ats.length > 0) ats.forEach(el=> msg = msg.replace(new RegExp(el,'i'), `<span style="padding: 0px; background: ${t_color.darkPurple}; border-radius: .3em;">${reg(new RegExp(el,'i').exec(msg),0)}</span>`) );
 
-        var chat= ele('span');
-        attr(chat,'style',`padding 2px; color: ${t_color.purpleLightGrey};`);
-        chat.innerText = arr[i].chat.trim();
- 		searchResCont.appendChild(chat);
+      var user= ele('span');
+      attr(user,'style',`font-weight: 700; padding 2px; color: ${userColor}`);
+      user.innerText = arr[i].user+': ';
+      searchResCont.appendChild(user);
+
+      var chat= ele('span');
+      attr(chat,'style',`padding 2px; color: ${t_color.purpleLightGrey}`);
+      chat.innerHTML = msg;
+      searchResCont.appendChild(chat);
     }
 }
+
 createChatSearchHTML()
