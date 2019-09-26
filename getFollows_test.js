758334121a629c83e478b21e2967712c5262bcff
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-26 16:21:32 -0400

    Update getFollows_test.js

--- getFollows_test.js
+++ getFollows_test.js
@@ -6,3 +6,19 @@ async function getFollows(){
 }
 
 getFollows()
+
+
+
+var reg = (o, n) => o ? o[n] : '';
+
+async function getFollows(){
+  var inputData = prompt("Paste the link to the user's profile here");
+  var path = reg(/\btwitch\.tv\/(.+)/i.exec(inputData),0) ? reg(/\btwitch\.tv\/(.+)/i.exec(inputData),0) : inputData ? inputData : '';
+  if(path){
+    var res = await fetch("https://gql.twitch.tv/gql", {"credentials":"include","headers":{"accept-language":"en-US","authorization":"OAuth j800v07qrzpvbdli78cjx8vbk7a3wb","client-id":"kimne78kx3ncx6brgo4mv6wki5h1ko","content-type":"text/plain;charset=UTF-8","sec-fetch-mode":"cors","x-device-id":"6ac4f47276ce23ef"},"referrer":"https://www.twitch.tv/"+path+"/following","referrerPolicy":"no-referrer-when-downgrade","body":"[{\"operationName\":\"ChannelFollows\",\"variables\":{\"limit\":50,\"login\":\""+path+"\",\"order\":\"DESC\"},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"4820aebc27595788fa5706403e083c616a854eff33ec8e90b078eaf7f2682b49\"}}}]","method":"POST","mode":"cors"});
+    var d = await res.json();
+    var follows = d[0].data.user.follows.edges.map(el=> el.node.displayName);
+    document.body.innerHTML = '<div style="padding: 50px;">'+ follows.reduce((a,b)=> a+'<br>'+b) +'</div>'
+  }
+}
+getFollows();
