2472e1b42ab5fa982ccfef3f1c8af5c2634ddeba
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-12-02 21:00:12 -0500

    Update getChatLogs_clientside.js

--- getChatLogs_clientside.js
+++ getChatLogs_clientside.js
@@ -17,15 +17,30 @@ var cleanObject = (ob) =>
 
 var containArr = [];
 
-var chatObj = Array.from(cn(document,'chat-line__message')).map(el=> {
-  var obj = {
-	t: cn(el,'chat-line__timestamp') && cn(el,'chat-line__timestamp').length ? cn(el,'chat-line__timestamp')[0].innerText : null,
-    u: cn(el,'chat-author__display-name') && cn(el,'chat-author__display-name').length ? cn(el,'chat-author__display-name')[0].innerText : null,
-    m: cn(el,'text-fragment') && cn(el,'text-fragment').length ? Array.from(cn(el,'text-fragment')).map(cc=> cc.innerText).reduce((a,b)=> a+b).trim() : null,
-    e: cn(el,'chat-image chat-line__message--emote tw-inline-block') && cn(el,'chat-image chat-line__message--emote tw-inline-block').length ? Array.from(cn(el,'chat-image chat-line__message--emote tw-inline-block')).map(cc=> cc.getAttribute('alt')) : null,
-  };
-  return cleanObject(obj);
-});
-chatObj.forEach(el=> {if(containArr.every(i=> JSON.stringify(i) != JSON.stringify(el))) containArr.push(el)});
+function readChats(){
+  var chatObj = Array.from(cn(document,'chat-line__message')).map(el=> {
+    var obj = {
+	  t: cn(el,'chat-line__timestamp') && cn(el,'chat-line__timestamp').length ? cn(el,'chat-line__timestamp')[0].innerText : null,
+      u: cn(el,'chat-author__display-name') && cn(el,'chat-author__display-name').length ? cn(el,'chat-author__display-name')[0].innerText : null,
+      c: cn(el,'text-fragment') && cn(el,'text-fragment').length ? Array.from(cn(el,'text-fragment')).map(cc=> cc.innerText).reduce((a,b)=> a+b).trim() : null,
+      e: cn(el,'chat-image chat-line__message--emote tw-inline-block') && cn(el,'chat-image chat-line__message--emote tw-inline-block').length ? Array.from(cn(el,'chat-image chat-line__message--emote tw-inline-block')).map(cc=> cc.getAttribute('alt')) : null,
+      m: cn(el,'mention-fragment') && cn(el,'mention-fragment').length ? Array.from(cn(el,'mention-fragment')).map(cc=> cc.innerText).reduce((a,b)=> a+b).trim() : null,
+    };
+    return cleanObject(obj);
+  });
+  chatObj.forEach(el=> {if(containArr.every(i=> JSON.stringify(i) != JSON.stringify(el))) containArr.push(el)});
 
-console.log(containArr)
+  if(JSON.stringify(containArr).length > 1000){
+    console.log(JSON.stringify(containArr).length);
+//     console.log(containArr);
+    containArr = [];
+  }
+}
+// console.log(containArr)
+
+var domObserver = new MutationObserver(() => readChats() );
+
+domObserver.observe(cn(document,'chat-list__list-container tw-flex-grow-1 tw-full-height tw-pd-b-1')[0], {
+  childList: true,
+  subtree: true
+});
