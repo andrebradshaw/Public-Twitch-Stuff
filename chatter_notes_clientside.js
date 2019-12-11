617ba685202db381f3a496c7d8a06b999818f8cb
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-12-11 12:38:08 -0500

    Update chatter_notes_clientside.js

--- chatter_notes_clientside.js
+++ chatter_notes_clientside.js
@@ -10,6 +10,13 @@ var attr = (o, k, v) => o.setAttribute(k, v);
 
 var a = (l, r) => r.forEach(a => attr(l, a[0], a[1]));
 
+var unqHsh = (a,o) => a.filter(i=> o.hasOwnProperty(i) ? false : (o[i] = true));
+
+var cleanObject = (ob) => 
+  Object.entries(ob).reduce((r, [k, v]) => {
+    if(v) { r[k] = v; return r;
+    } else { return r; }
+  }, {});
 
 
 var svgs = {
@@ -128,6 +135,9 @@ function createOptionHTML(){
   noteCont.appendChild(add);
   add.onclick = sendToSheets;
   
+  var msgObj = getLastChatObj(this.parentElement);
+
+  console.log(msgObj);
 }
 
 function createNotesHistoryHTML(){
@@ -190,6 +200,20 @@ function createNotesHistoryHTML(){
   }
   
 }
+
+
+function getLastChatObj(el){
+  var chatObj = el ? cleanObject({
+	  time: cn(el,'chat-line__timestamp') && cn(el,'chat-line__timestamp').length ? cn(el,'chat-line__timestamp')[0].innerText : null,
+      user: cn(el,'chat-author__display-name') && cn(el,'chat-author__display-name').length ? cn(el,'chat-author__display-name')[0].innerText : null,
+      chat: cn(el,'text-fragment') && cn(el,'text-fragment').length ? Array.from(cn(el,'text-fragment')).map(cc=> cc.innerText).reduce((a,b)=> a+b).trim() : null,
+      emotes: cn(el,'chat-image chat-line__message--emote tw-inline-block') && cn(el,'chat-image chat-line__message--emote tw-inline-block').length ? Array.from(cn(el,'chat-image chat-line__message--emote tw-inline-block')).map(cc=> cc.getAttribute('alt')) : null,
+      mentions: cn(el,'mention-fragment') && cn(el,'mention-fragment').length ? Array.from(cn(el,'mention-fragment')).map(cc=> cc.innerText) : null,
+      links: cn(el,'link-fragment tw-interactive tw-link tw-link--button') && cn(el,'link-fragment tw-interactive tw-link tw-link--button').length ? Array.from(cn(el,'link-fragment tw-interactive tw-link tw-link--button')).map(cc=> cc.innerText) : null,
+    }) : null;
+  return chatObj;
+}
+
 
 function sendToSheets(){
   var content = tn(this.parentElement,'textarea') && tn(this.parentElement,'textarea').length ? tn(this.parentElement,'textarea')[0].value : '';
